e758aa523b83d3ca1e0615bd64fee924c398c170
Anisha Zaveri <anishazaveri@gmail.com>
2021-11-08 14:22:20 -0500

    Removed readmepypi from setup due to install error

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README_PyPi.md").read_text()
 
 setup(name='austen_plots',
-      version='0.1.2',
+      version='0.1.4',
       description='Sensitivity analysis for causal inference',
       url='https://github.com/anishazaveri/austen_plots',
       author='Anisha Zaveri',
       author_email='anishazaveri@gmail.com',
       license='MIT',
       packages=['austen_plots'],
-      long_description=long_description,
+      long_description="See [GitHub repo](https://github.com/anishazaveri/austen_plots) for more information",
       long_description_content_type='text/markdown',
       classifiers=[
         "License :: OSI Approved :: MIT License",
