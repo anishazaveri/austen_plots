51bccb6f9df028e13b4f0b660d738e162683bda1
Anisha Zaveri <anishazaveri@gmail.com>
2021-11-08 12:38:05 -0500

    Changed readme for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README_PyPi.md").read_text()
 
 setup(name='austen_plots',
-      version='0.1.0',
+      version='0.1.2',
       description='Sensitivity analysis for causal inference',
       url='https://github.com/anishazaveri/austen_plots',
       author='Anisha Zaveri',
       author_email='anishazaveri@gmail.com',
       license='MIT',
       packages=['austen_plots'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
